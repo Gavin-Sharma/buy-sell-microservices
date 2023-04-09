1ccfd35c3b07ebaeaf0c63bc0418ac21ed65e54a
Gavin Sharma <50966530+wow-gavin-codes-fast@users.noreply.github.com>
2023-04-08 21:43:55 -0700

    update

--- Dashboard/src/components/Stats.js
+++ Dashboard/src/components/Stats.js
@@ -1,28 +1,51 @@
-import { useEffect, useState, useRef } from 'react'
+import { useState, useEffect } from 'react';
 
-const Stats = () => {
-    const [event, setEvent] = useState([])
+const Dashboard = () => {
+  const [statuses, setStatuses] = useState({});
+  const [stats, setStats] = useState({});
 
-    useEffect(() => {
-        fetch('http://52.39.252.17/processing/stats')
-        .then(res => res.json())
-        .then(res => { 
-            setEvent(res) 
-        })
-    }, [])
+  const fetchStatuses = () => {
+    fetch('http://52.39.252.17/health')
+      .then(response => response.json())
+      .then(data => {
+        setStatuses(data);
+      })
+      .catch(error => console.log(error));
+  };
 
-    return (
-        <div className="stats">
-            <h2>Latest Statistics</h2>
-            <div>
-                <p>Max Buy Price: {event.max_buy_price}</p>
-                <p>Number of Buys: {event.num_buys}</p>
-                <p>Max Sell Price: {event.max_sell_price}</p>
-                <p>Number of Sells: {event.num_sells}</p>
-                <p>Last Updated: {event.max_buy_price}</p>
-            </div>
-        </div>
-    )   
-}
+  useEffect(() => {
+    fetch('http://52.39.252.17/processing/stats')
+      .then(response => response.json())
+      .then(data => {
+        setStats(data);
+      })
+      .catch(error => console.log(error));
+  }, []);
 
-export default Stats
+  return (
+    <div className="dashboard">
+      <h1>Dashboard</h1>
+      <div className="stats">
+        <h2>Latest Statistics</h2>
+        <ul>
+          <li>Max Buy Price: {stats.max_buy_price}</li>
+          <li>Number of Buys: {stats.num_buys}</li>
+          <li>Max Sell Price: {stats.max_sell_price}</li>
+          <li>Number of Sells: {stats.num_sells}</li>
+          <li>Last Updated: {stats.last_updated}</li>
+        </ul>
+
+        <h2>Statuses:</h2>
+        <button onClick={fetchStatuses}>Check Statuses</button>
+        <ul>
+          <li>Receiver: {statuses.receiver}</li>
+          <li>Storage: {statuses.storage}</li>
+          <li>Processing: {statuses.processing}</li>
+        </ul>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Dashboard;
